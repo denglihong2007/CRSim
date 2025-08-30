57fd8e42542d9c816992851147318795859cf7cc
denglihong2007 <98096191+denglihong2007@users.noreply.github.com>
2025-08-30 17:29:32 +0800

    解决冲突

--- CRSim/ViewModels/DashboardPageViewModel.cs
+++ CRSim/ViewModels/DashboardPageViewModel.cs
@@ -3,29 +3,9 @@
 namespace CRSim.ViewModels;
 public partial class DashboardPageViewModel : ObservableObject
 {
-    [ObservableProperty]
-    public partial string? UpdateMessage { get; set; } = null;
     public StyleManager StyleManager { get; }
-    private readonly INetworkService _networkService;
-    private readonly ISettingsService _settingsService;
-    public DashboardPageViewModel(StyleManager styleManager,INetworkService networkService,ISettingsService settingsService)
+    public DashboardPageViewModel(StyleManager styleManager)
     {
         StyleManager = styleManager;
-        _networkService = networkService;
-        _settingsService = settingsService;
-        InitializeAsync();
-    }
-    private async void InitializeAsync()
-    {
-        var url = _settingsService.GetSettings().ApiUri.Contains("47")
-            ? "https://api.github.com/repos/denglihong2007/CRSim/releases/latest"
-            : "https://crsim.com.cn/api/version";
-
-        var updateInfo = await _networkService.GetUpdateAsync(url);
-
-        if (updateInfo is not null && updateInfo.Name != Assembly.GetExecutingAssembly().GetName().Version.ToString())
-        {
-            UpdateMessage = $"有新版本 {updateInfo.Name} 可用，请前往“设置”下载安装更新！";
-        }
     }
 }
