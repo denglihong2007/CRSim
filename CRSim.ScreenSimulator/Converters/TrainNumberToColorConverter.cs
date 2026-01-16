9661d723f70fd8c932fc3b91eff0840338fbd4ac
denglihong2007 <98096191+denglihong2007@users.noreply.github.com>
2026-01-16 21:47:18 +0800

    feat: 统一车次号颜色设置

---
+++ CRSim.ScreenSimulator/Converters/TrainNumberToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using CRSim.Core.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CRSim.ScreenSimulator.Converters
+{
+    public class TrainNumberToColorConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var _settings = StyleManager.ServiceProvider
+                .GetRequiredService<ISettingsService>()
+                .GetSettings();
+            var trainColors = _settings.TrainColors;
+            if (value is string trainNumber)
+            {
+                var matchedColor = trainColors
+                    .Where(tc => trainNumber.StartsWith(tc.Prefix))
+                    .OrderByDescending(tc => tc.Prefix.Length)
+                    .FirstOrDefault();
+                var color = matchedColor?.Color ?? trainColors.FirstOrDefault(x => x.Prefix == "默认").Color;
+                var mediaColor = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+                return new SolidColorBrush(mediaColor);
+            }
+            return new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
